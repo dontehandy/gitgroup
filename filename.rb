13d3ec4c9f7c78948bbc22b94493b1e2322bae29
Donte Handy <donte.handy@gmail.com>
2024-12-19 12:47:23 -0400

    changed 1 to a

--- filename.rb
+++ filename.rb
@@ -1,8 +1,8 @@
 Class ExampleClass
-    attr_reader :attribute1, :attribute2, :attribute3
+    attr_reader :attributea, :attribute2, :attribute3
 
-    def initialize(attribute1, attribute2, attribute3)
-        @attribute1 = attribute1
+    def initialize(attributea, attribute2, attribute3)
+        @attributea = attributea
         @attribute2 = attribute2
         @attribute3 = attribute3
     end
