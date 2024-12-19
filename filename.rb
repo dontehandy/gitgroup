4d0358689f368a32828ea8dbad16728e7f5f8db5
Donte Handy <donte.handy@gmail.com>
2024-12-19 12:44:20 -0400

    initial commit

---
+++ filename.rb
@@ -0,0 +1,10 @@
+Class ExampleClass
+    attr_reader :attribute1, :attribute2, :attribute3
+
+    def initialize(attribute1, attribute2, attribute3)
+        @attribute1 = attribute1
+        @attribute2 = attribute2
+        @attribute3 = attribute3
+    end
+end
+
